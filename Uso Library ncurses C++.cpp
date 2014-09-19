b0998eed6d390c7da79715d072d66743b1f555fe
AguHDz <AguHDz@users.noreply.github.com>
2014-09-19 23:52:27 +0200

    Update Uso Library ncurses C++.cpp

--- Uso Library ncurses C++.cpp
+++ Uso Library ncurses C++.cpp
@@ -3,6 +3,9 @@
 // Libreria estandar de C++ que reemplaza a la antigua conio.h
 // http://www.tldp.org/HOWTO/NCURSES-Programming-HOWTO/helloworld.html
 
+// Más información sobre el uso y utilidad de la libreria ncurses.h en:
+// http://www.juntadeandalucia.es/averroes/~41009822/inf/dfsi/Iniciacion%20a%20ncurses.pdf
+
 int main()
 {	
 	initscr();			/* Start curses mode 		  */
